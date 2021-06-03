a96c748ecfb56657d8ff6b94504cc80b7844befa
Sheron Suditha <github@sudithakumarasinghe.me>
2021-06-04 02:34:07 +0530

    new(mongodb): init : #failing code not completed

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+import flask
 from flask import Flask
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
 
 app = Flask(__name__)
 
+mongodb_client = PyMongo(app, uri={os.getenv("MONGO_URL")})
 
 @app.route('/get-update', methods=['GET'])
 def handle_update():
@@ -19,5 +26,16 @@ def hello_world():
     }
 
 
+@app.route('/get-db', methods=['GET'])
+def get_from_db():
+    try:
+        recipes = mongodb_client.SIT.find({})
+        print(recipes)
+        return flask.jsonify([recipe for recipe in recipes])
+    except:
+        print("error")
+
+
 if __name__ == '__main__':
     app.run()
+
